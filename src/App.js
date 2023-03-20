fc55ec00958800b5228dabd0d82e068193013031
Ryan Eastman <rking1323@gmail.com>
2023-03-20 22:49:22 +0000

    setup for API retrieval

--- src/App.js
+++ src/App.js
@@ -3,13 +3,32 @@ import Axios from 'axios';
 import './App.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cityData: [],
+    }
+  }
+
+// ********** GET CITY DATA **********
+
+handleGetCityData = () => {
+  event.preventDefault();
+
+  // TODO: USE AXIOS TO MAKE A CALLOUT TO THE LOCATION API
+  let cityData = axios.get(URL);
+
+  // TODO: SET THAT DATA INTO STATE
+
+}
+
   render() {
     return (
       <>
       <h1>API Calls</h1>
 
       <form>
-        <button type= 'submit'>Cities</button>
+        <button type= 'submit' onClick= {this.handleGetCityData}>Cities</button>
       </form>
       </>
     )

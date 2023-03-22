12552656f5891d0f1a717dfa85c09c0e7ae121e2
Ryan Eastman <rking1323@gmail.com>
2023-03-22 23:19:29 +0000

    fixed the state issue I was having last night

--- src/Weather.js
+++ src/Weather.js
@@ -2,25 +2,21 @@ import React from 'react';
 
 class Weather extends React.Component {
   render() {
-
     return (
       <>
-        {this.props.weatherData.map(day => {
+        {this.props.description.map((day, idx) => {
           return (
-            <>
+            <div key= {idx}>
               <h1>{day.date}</h1>
               <p>{day.description}</p>
-            </>
+            </div>
           )
         })}
 
       </>
     )
-
-
-
-
   }
 }
+
 
 export default Weather;
